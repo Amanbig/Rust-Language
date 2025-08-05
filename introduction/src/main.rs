21faa29a3d8c9ffccd18ac044f16f5d58b5b4938
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-05 22:16:03 +0530

    added initial introduction about rust language with print and println macro

---
+++ introduction/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // fn is a keyword used to define a function
+    // main is the name of the function that seves as the entry point of the program
+    println!("Hello, world!"); // println! is a macro that prints to the console with a newline
+
+    print!("Hello, world!"); // print! is a macro that prints to the console without a newline
+
+}
