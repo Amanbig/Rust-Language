9ddc9c0844dd43b54b090b91db465523a2604a25
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-05 22:39:44 +0530

    added constants

---
+++ constants/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // constants are used to store fixed values
+    // const is a keyword used to declare a constant
+    // constant values cannot be changed
+    // constants must have a type annotations
+    // always declare constants in uppercase
+    const PI: f64 = 3.145;
+    println!("The value of PI is: {}",PI);
+}
